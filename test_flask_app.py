e0be489f260046ab4955628c81f1e610c02cfa04
Bernabe Jimenez <bernabejimenez@MacBook-Pro-de-Bernabe.local>
2020-05-26 00:38:05 +0200

    devuelve JSON

--- test_flask_app.py
+++ test_flask_app.py
@@ -6,10 +6,10 @@ class TestWeb(TestFlaskBase):
             response = self.client.get("/")
             self.assertEqual(response.status_code,200)
     
-    def test_route_index_is_Hola_mund(self):
+    def test_route_index_is_lista_regiones(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code,200)
-        self.assertEqual(response.data, b'Hola, mundo')
+        
     
 
 if __name__ == '__main__':
